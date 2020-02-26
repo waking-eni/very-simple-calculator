d55e8ab1559afc1288c9abf1e0ca43b34cb42b3f
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-02-26 16:20:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>prvi</title>
+</head>
+<body>
+
+<form action="kalkulator.php" method="post">
+	<input type="text" name="num1">
+	<input type="text" name="num2">
+	<label for="operat">Choose an operation:</label>
+	<select id="operat" name="operat">
+		<option value="0">Addition</option>
+		<option value="1">Subtraction</option>
+		<option value="2">Multiplication</option>
+		<option value="3">Division</option>
+	</select>
+	<button type="submit" name="do-operation">Do operation</button>
+	<p id="ispis"></p>
+</form>
+
+<script>
+var opt = document.getElementById('operat');
+console.log( opt.value );
+</script>
+</body>
+</html>
